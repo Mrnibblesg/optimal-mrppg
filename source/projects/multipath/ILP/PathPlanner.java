7563b044c05289650c058c698b4ca53f3fa6d02f
Parker Gutierrez <parkerg444@gmail.com>
2026-03-19 12:03:02 -0700

    Fix imports for gurobi1301

--- source/projects/multipath/ILP/PathPlanner.java
+++ source/projects/multipath/ILP/PathPlanner.java
@@ -1,6 +1,6 @@
 package projects.multipath.ILP;
 
-import gurobi.GRBException;
+import com.gurobi.gurobi.GRBException;
 import projects.multipath.advanced.Graph;
 import projects.multipath.advanced.Vertex;
 
