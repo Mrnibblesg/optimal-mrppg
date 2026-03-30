d3b0ccaf8028e5df6d8a35a073deb75af75b5ef6
Parker Gutierrez <parkerg444@gmail.com>
2026-03-30 00:47:22 -0700

    Create a hello world entry point

---
+++ source/projects/multipath/Server/Server.java
@@ -0,0 +1,11 @@
+package projects.multipath.Server;
+
+import projects.multipath.ILP.MultiagentGraphSolverGurobiTime;
+
+
+public class Server{
+    public static void main(String[] args){
+        //Read from protobuf queue and start jobs
+        System.out.println("Hello World!");
+    }
+}
